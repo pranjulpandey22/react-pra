27adf288b804ed84d12e40b315251a80ac887dc8
pranjulpandey22 <pranjulpandey22@gmail.com>
2024-02-21 13:03:08 +0530

    hello

---
+++ App.js
@@ -0,0 +1,3 @@
+const heading = React.createElement('h1',{id:'headi',xyz:"heading"},'hello react from local system for config')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading)
